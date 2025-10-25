3e8a95bda7fcf7ed916199ca53c1485779546d24
Nandini <nandini.sivakumari@gmail.com>
2025-10-25 10:52:58 +0530

    first

---
+++ server.js
@@ -0,0 +1,10 @@
+console.log("888i");
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    return res.send("helllll0000")
+})
+
+const port = 3000
+app.listen(port, () => console.log(`Server runnind at ${port}`))
