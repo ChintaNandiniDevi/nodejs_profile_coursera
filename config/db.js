660072806447632301cf990d94b5b07ae108cbb0
Nandini <nandini.sivakumari@gmail.com>
2025-10-25 13:15:45 +0530

    mongo db connected

---
+++ config/db.js
@@ -0,0 +1,14 @@
+const config = require('config');
+const mongoose = require('mongoose')
+const mongoUrl = config.get('mongoURL');
+
+const connectDB = async() => {
+    try{
+        await mongoose.connect(mongoUrl);
+        console.log("mongo connected")
+    } catch(err) {
+        console.log("server error")
+    }
+}
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 console.log("888i");
 const express = require('express');
 const app = express();
+const connectDB = require('./config/db');
+
+connectDB();
 
 app.get('/', (req, res) => {
     return res.send("helllll0000")
